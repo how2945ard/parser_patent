9554e192f1543215c38fa106ae9032b012bae450
how2945ard <how2945ard@gmail.com>
2016-03-19 16:27:04 +0800

    first

---
+++ app.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var parser = require('./parser');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var _ = require('lodash');
+var Promise = require('bluebird');
+
+exports.server = function() {
+  var app = express();
+
+  app.set('port', 3000);
+
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'dcard'
+  });
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+  app.post('/', function(req, res) {
+    var insertOne = function(num) {
+      return new Promise(function(resolve, reject) {
+        connection.query('INSERT INTO `Temp_link`(`serial_number`) VALUES (' + num + ')', function(err, rows, fields) {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(num);
+        });
+      });
+    };
+    var term = req.body.term;
+    return parser(term)
+      .then(function(result) {
+        PromiseArray = _.map(result, function(element) {
+          return insertOne(element);
+        });
+        return Promise.all(PromiseArray);
+      })
+      .then(function(results) {
+        return res.json(results);
+      });
+  });
+
+  var server = http
+    .createServer(app)
+    .listen(app.get('port'), function() {
+      connection.connect();
+      console.log('Express server listening on port ' + app.get('port'));
+    });
+
+  process.on('exit', function() {
+    connection.end();
+  });
+};
---
+++ parser.js
@@ -0,0 +1,68 @@
+var cheerio = require('cheerio');
+var request = require('request');
+var Promise = require('bluebird');
+var _ = require('lodash');
+Promise.promisifyAll(request);
+
+
+function getSerialNumberInAPage(pageNum, search) {
+  var url = 'http://appft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&u=%2Fnetahtml%2FPTO%2Fsearch-adv.html&r=0&f=S&l=50&d=PG01&s1=VR&s2=APPLE&co1=AND&Page=Next';
+
+  function setPage(pageNum) {
+    url += '&p=' + pageNum;
+    return url;
+  }
+
+  function searchTerm(search) {
+    url += '&OS=' + search;
+    url += '&RS=' + search;
+    return url;
+  }
+
+  setPage(pageNum);
+  searchTerm(search);
+
+  return request
+    .getAsync(url)
+    .then(function(request) {
+      var body = request.body;
+      var $ = cheerio.load(body);
+      var total = $('body strong:nth-child(3)').text();
+
+      var numberArray = [];
+      $('body table')
+        .find('tr')
+        .map(function(index, row) {
+          var element = $(this).find($('td:nth-child(2)')).text();
+          numberArray.push(element);
+        });
+      return {
+        numbers: _.drop(numberArray, 1),
+        total: total
+      };
+    });
+}
+
+function getAllSerialNumer(search) {
+  var serialNumbers = [];
+  return getSerialNumberInAPage(1, search)
+    .then(function(result) {
+      var last = Math.floor(result.total / 50) + 1;
+      var currentPage = 2;
+      var PromiseArray = [];
+      serialNumbers = serialNumbers.concat(result.numbers);
+      while (currentPage <= last + 1) {
+        PromiseArray.push(getSerialNumberInAPage(currentPage));
+        currentPage += 1;
+      }
+      return Promise.all(PromiseArray);
+    })
+    .then(function(results) {
+      _.each(results, function(result) {
+        serialNumbers = serialNumbers.concat(result.numbers);
+      });
+      return serialNumbers;
+    });
+}
+
+module.exports = getAllSerialNumer;
